e1e5f5807930262819b8758f7dff4e2da167f74a
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-08-16 06:17:18 +0300

    Added database mongodb

--- create_bot.py
+++ create_bot.py
@@ -3,9 +3,11 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram import Bot
 from os import getenv
 from dotenv import load_dotenv
+from scripts import Database
 
 load_dotenv()
 
 bot = Bot(token=getenv('TOKEN'))
+db = Database(getenv("CONNSTRING"))
 
 dp = Dispatcher(bot, storage=MemoryStorage())
---
+++ scripts/mongo.py
@@ -0,0 +1,18 @@
+import pymongo
+
+
+class Database:
+    def __init__(self, connstring):
+        self.__connection = pymongo.MongoClient(connstring)
+        self.dbUser = self.__connection["test"]
+        self.collUser = self.dbUser["users"]
+
+    async def userAdd(self, user_id, email, password):
+        self.collUser.insert_one(
+            {"_id": user_id, "email": email, "password": password})
+
+    async def userExsist(self, user_id):
+        return self.collUser.find_one({"_id": user_id})
+
+    async def userInfo(self, user_id):
+        return self.collUser.find_one({"_id": user_id})
