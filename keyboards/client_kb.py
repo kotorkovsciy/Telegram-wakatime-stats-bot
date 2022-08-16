3745ff27295a827b8ea05364ae8442c2c07065ca
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-08-16 20:43:29 +0300

    Update keyboards

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -6,10 +6,14 @@ kb_reg = ReplyKeyboardMarkup(resize_keyboard=True)
 kb_reg.add(but_reg)
 
 
-but_stat_yp = KeyboardButton('Статистика по яп')
+but_all_time = KeyboardButton('Всё время')
+but_stat_lang = KeyboardButton('Статистика по языкам')
 but_stat_os = KeyboardButton('Статистика по ос')
+but_stat_editors = KeyboardButton('Статистика по редакторам')
+but_stat_categories = KeyboardButton('Статистика по категориям')
 kb_stats = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_stats.add(but_stat_yp).add(but_stat_os)
+kb_stats.add(but_all_time).add(but_stat_lang).add(but_stat_os)\
+    .add(but_stat_editors).add(but_stat_categories)
 
 
 async def kb_client(Krivda):
