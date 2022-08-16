5226870bd0d769c20a3188f43b8bf1fc57967db8
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-08-16 20:43:09 +0300

    📉 Added more statistics 📈

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,4 @@
 from scripts.mongo import Database
 from scripts.Auth_Wakatime import acc_verify, browsedriver
-from scripts.analitic_stats import lang_stats
+from scripts.analitic_stats import lang_stats, os_stats, editors_stats, categories_stats, all_time
 from scripts.wakatime import wakatime_stats
--- scripts/analitic_stats.py
+++ scripts/analitic_stats.py
@@ -1,6 +1,6 @@
 from scripts.wakatime import wakatime_stats
 import pandas as pd
-import json
+from json import dump
 import matplotlib.pyplot as plt
 from os import remove
 
@@ -8,15 +8,15 @@ from os import remove
 async def record(filename, email, password):
     """Запись статистик"""
     with open(filename, 'w') as f:
-        json.dump(await wakatime_stats(email, password), f)
+        dump(await wakatime_stats(email, password), f)
 
 
 async def lang_stats(user_id, email, password):
 
-    await record(f"{user_id}.json", email, password)
+    await record(f"info/json/{user_id}.json", email, password)
 
-    workouts = pd.read_json(f"{user_id}.json")
-    remove(f'{user_id}.json')
+    workouts = pd.read_json(f"info/json/{user_id}.json")
+    remove(f'info/json/{user_id}.json')
     yp = workouts["data"]["languages"]
     yps, times = [], []
     other = 0
@@ -35,7 +35,82 @@ async def lang_stats(user_id, email, password):
     fig, ax = plt.subplots()
     ax.pie(times, labels=yps, shadow=True)
     ax.axis("equal")
-    plt.savefig(f"{user_id}")
+    plt.savefig(f"info/images/{user_id}_lang_stats")
 
-    img = open(f'{user_id}.png', "rb")
+    img = open(f'info/images/{user_id}_lang_stats.png', "rb")
     return img
+
+
+async def os_stats(user_id, email, password):
+
+    await record(f"info/json/{user_id}.json", email, password)
+
+    workouts = pd.read_json(f"info/json/{user_id}.json")
+    remove(f'info/json/{user_id}.json')
+    os = workouts["data"]["operating_systems"]
+    oss, times = [], []
+
+    for i in os:
+        times.append(i["total_seconds"])
+        oss.append(i["name"])
+
+    fig, ax = plt.subplots()
+    ax.pie(times, labels=oss, shadow=True)
+    ax.axis("equal")
+    plt.savefig(f"info/images/{user_id}_os_stats")
+
+    img = open(f'info/images/{user_id}_os_stats.png', "rb")
+    return img
+
+
+async def editors_stats(user_id, email, password):
+
+    await record(f"info/json/{user_id}.json", email, password)
+
+    workouts = pd.read_json(f"info/json/{user_id}.json")
+    remove(f'info/json/{user_id}.json')
+    edit = workouts["data"]["editors"]
+    editors, times = [], []
+
+    for i in edit:
+        times.append(i["total_seconds"])
+        editors.append(i["name"])
+
+    fig, ax = plt.subplots()
+    ax.pie(times, labels=editors, shadow=True)
+    ax.axis("equal")
+    plt.savefig(f"info/images/{user_id}_editors_stats")
+
+    img = open(f'info/images/{user_id}_editors_stats.png', "rb")
+    return img
+
+
+async def categories_stats(user_id, email, password):
+
+    await record(f"info/json/{user_id}.json", email, password)
+
+    workouts = pd.read_json(f"info/json/{user_id}.json")
+    remove(f'info/json/{user_id}.json')
+    categor = workouts["data"]["categories"]
+    categories, times = [], []
+
+    for i in categor:
+        times.append(i["total_seconds"])
+        categories.append(i["name"])
+
+    fig, ax = plt.subplots()
+    ax.pie(times, labels=categories, shadow=True)
+    ax.axis("equal")
+    plt.savefig(f"info/images/{user_id}_categories_stats")
+
+    img = open(f'info/images/{user_id}_categories_stats.png', "rb")
+    return img
+
+
+async def all_time(user_id, email, password):
+
+    await record(f"info/json/{user_id}.json", email, password)
+
+    workouts = pd.read_json(f"info/json/{user_id}.json")
+    remove(f'info/json/{user_id}.json')
+    return workouts["data"]["human_readable_total_including_other_language"]
