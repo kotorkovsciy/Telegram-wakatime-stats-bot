c08dfaa84160212d7065aa3cd515666d0a95f624
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-08-16 06:21:57 +0300

    Update keyboards

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,6 +1,25 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-but_start = KeyboardButton('start')
 
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_client.add(but_start)
+but_reg = KeyboardButton('Регистрация')
+kb_reg = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_reg.add(but_reg)
+
+
+but_stat_yp = KeyboardButton('Статистика по яп')
+but_stat_os = KeyboardButton('Статистика по ос')
+kb_stats = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_stats.add(but_stat_yp).add(but_stat_os)
+
+
+async def kb_client(Krivda):
+    if not Krivda:
+        return kb_reg
+    else:
+        return kb_stats
+
+
+but_cancel = KeyboardButton('Отмена')
+
+kb_cancel = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_cancel.add(but_cancel)
