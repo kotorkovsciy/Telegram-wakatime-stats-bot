c65cdc2d8aa2dae311ccd2b7afe210f3c468fcda
kotorkovsciy <kotorkovsciy@gmail.com>
2022-08-22 10:57:11 +0300

    add logging

--- bot.py
+++ bot.py
@@ -5,13 +5,18 @@ from scripts import browsedriver
 from handlers import client, stats
 from os.path import exists
 from os import mkdir
-
+from logging import basicConfig, INFO
+from datetime import datetime as dt
+from create_bot import db
 
 async def on_startup(_):
+    if not exists("logs/"):
+        mkdir("logs/")
     if not exists("info/"):
         mkdir("info/")
         mkdir("info/images/")
         mkdir("info/json/")
+    basicConfig(filename=f'logs/[{dt.today().strftime("%Y-%m-%d-%H")}].log', filemode='a', level=INFO)
     await browsedriver()
     print('bot online!')
 
