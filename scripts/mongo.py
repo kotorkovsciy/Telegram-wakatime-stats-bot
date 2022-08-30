f3d5e49681f73a0c164c44a79d61bce3b590ea90
kotorkovsciy <kotorkovsciy@gmail.com>
2022-08-30 23:40:14 +0300

    correcting bot

--- scripts/mongo.py
+++ scripts/mongo.py
@@ -2,6 +2,16 @@ import pymongo
 
 
 class Database:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __del__(self):
+        Database.__instance = None
+
     def __init__(self, connstring):
         """Инициализация подключения к базе данных"""
         self.__connection = pymongo.MongoClient(connstring)
