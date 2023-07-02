a1d4f398554ae6f79e7dfec52c76cc146ba7b0bc
Kotorkovsciy <kotorkovsciy@gmail.com>
2023-07-02 22:16:23 +0300

    feat: Corrected init file for scripts

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,3 @@
 from scripts.mongo import Database
-from scripts.Auth_Wakatime import acc_verify, browsedriver
 from scripts.analitic_stats import AnaliticStats, NotifyStats
-from scripts.wakatime import wakatime_stats
+from scripts.wakatime import WakatimeAPI
