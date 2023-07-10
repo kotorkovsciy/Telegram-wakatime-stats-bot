7234f6e0f08c073a116d0d02d40fe8b0d7c942ce
Kotorkovsciy <kotorkovsciy@gmail.com>
2023-07-10 07:25:45 +0300

    feat: Added collection with statistics
    Made methods for reading and writing statistics
    
    When recording statistics, the user id, the day of recording, a dictionary with statistics, and the subject of statistics are recorded.

--- scripts/mongo.py
+++ scripts/mongo.py
@@ -1,4 +1,5 @@
 import pymongo
+from pymongo import DESCENDING
 
 
 class Database:
@@ -17,6 +18,7 @@ class Database:
         self.__connection = pymongo.MongoClient(connstring)
         self.__dbUser = self.__connection["test"]
         self.__collUser = self.__dbUser["users"]
+        self.__collStats = self.__dbUser["stats"]
 
     async def userAdd(self, user_id, refresh_token):
         """ "Добавление пользователя"""
@@ -37,3 +39,26 @@ class Database:
     async def AllUser(self):
         """Получение всех пользователей"""
         return self.__collUser.find()
+
+    async def add_stats(self, user_id, date, stats, theme):
+        """Добавление статистики"""
+        _id = self.__collStats.find().sort("_id", DESCENDING).limit(1)
+
+        if self.__collStats.count_documents({}) == 0:
+            _id = 0
+        else:
+            _id = _id[0]["_id"] + 1
+
+        self.__collStats.insert_one(
+            {
+                "_id": _id,
+                "user_id": user_id,
+                "date": date,
+                "stats": stats,
+                "theme": theme
+            }
+        )
+
+    async def get_stats(self, user_id, theme):
+        """Получение статистики"""
+        return self.__collStats.find({"user_id": user_id, "theme": theme})
