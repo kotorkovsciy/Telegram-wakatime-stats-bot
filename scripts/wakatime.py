bc412b1e9550121e0cd030437949d49b997d6087
Nikita <98780866+kotorkovsciy@users.noreply.github.com>
2023-07-12 07:13:56 +0000

    docs(wakatime): Written docstrings for methods

--- scripts/wakatime.py
+++ scripts/wakatime.py
@@ -5,6 +5,29 @@ import os
 
 
 class WakatimeAPI:
+    """
+    Class for work with WakaTime API
+
+    https://wakatime.com/developers
+
+    Attributes:
+        client_id (str): Client ID from WakaTime
+        client_secret (str): Client Secret from WakaTime
+        session (rauth.session): Session for work with WakaTime API
+        authorize_url (str): URL for authorization
+        access_token_url (str): URL for get access token
+        base_url (str): Base URL for WakaTime API
+        redirect_uri (str): Redirect URI for WakaTime API
+        headers (dict): Headers for WakaTime API
+
+
+    Methods:
+        get_url_auth: Get URL for authorization
+        set_auth_session: Set session for work with WakaTime API
+        refresh_session: Refresh session for work with WakaTime API
+        get_refresh_token: Get refresh token
+        get_access_token: Get access token
+    """
     session = None
     authorize_url = "https://wakatime.com/oauth/authorize"
     access_token_url = "https://wakatime.com/oauth/token"
@@ -12,7 +35,7 @@ class WakatimeAPI:
     redirect_uri = "https://wakatime.com/oauth/test"
     headers = {"Accept": "application/x-www-form-urlencoded"}
 
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret) -> None:
         self.client_id = client_id
         self.client_secret = client_secret
 
@@ -26,7 +49,14 @@ class WakatimeAPI:
         )
         self.state = hashlib.sha1(os.urandom(40)).hexdigest()
 
-    def get_url_auth(self):
+    def get_url_auth(self) -> str:
+        """Get URL for authorization
+
+        https://wakatime.com/developers#authentication
+
+        Returns:
+            str: URL for authorization
+        """
         params = {
             "scope": "email,read_stats",
             "response_type": "code",
@@ -38,7 +68,18 @@ class WakatimeAPI:
 
         return url
 
-    def set_auth_session(self, code):
+    def set_auth_session(self, code) -> bool:
+        """Set session for work with WakaTime API
+
+        https://wakatime.com/developers#authentication
+
+        Args:
+            code (str): Code for authorization
+        
+        Returns:
+            bool: True if session is set, else False
+        """
+
         try:
             self.session = self.service.get_auth_session(
                 headers=self.headers,
@@ -53,7 +94,11 @@ class WakatimeAPI:
 
         return True
 
-    def refresh_session(self):
+    def refresh_session(self) -> None:
+        """Refresh session for work with WakaTime API
+        
+        https://wakatime.com/developers#authentication
+        """
         refresh_token = dict(parse_qsl(self.session.access_token_response.text))[
             "refresh_token"
         ]
@@ -67,7 +112,15 @@ class WakatimeAPI:
 
         self.session = self.service.get_auth_session(data=data)
 
-    def _new_refresh_session(self, refresh_token):
+    def _new_refresh_session(self, refresh_token) -> None:
+        """Refresh session for work with WakaTime API
+
+        https://wakatime.com/developers#authentication
+
+        Args:
+            refresh_token (str): Refresh token
+        """
+
         data = {
             "grant_type": "refresh_token",
             "client_id": self.client_id,
@@ -77,22 +130,67 @@ class WakatimeAPI:
 
         self.session = self.service.get_auth_session(data=data)
 
-    def get_refresh_token(self):
+    def get_refresh_token(self) -> str:
+        """Get refresh token
+
+        https://wakatime.com/developers#authentication
+
+        Returns:
+            str: Refresh token
+        """
         return dict(parse_qsl(self.session.access_token_response.text))["refresh_token"]
 
-    def get_access_token(self):
+    def get_access_token(self) -> str:
+        """Get access token
+
+        https://wakatime.com/developers#authentication
+
+        Returns:
+            str: Access token
+        """
         return dict(parse_qsl(self.session.access_token_response.text))["access_token"]
 
 
 class WakatimeStats(WakatimeAPI):
-    def __init__(self, client_id, client_secret):
+    """
+    Class based class WakatimeAPI for get statistics from WakaTime API
+
+    https://wakatime.com/developers
+    
+    Methods:
+        get_stats: Get all statistics
+        get_lang_stats: Get statistics by languages
+        get_all_time: Get all time
+        get_os_stats: Get statistics by operating systems
+        get_editors_stats: Get statistics by editors
+        get_categories_stats: Get statistics by categories
+    """
+    def __init__(self, client_id, client_secret) -> None:
         super(WakatimeStats, self).__init__(client_id, client_secret)
 
-    def get_stats(self):
+    def get_stats(self) -> dict:
+        """Get statistics
+
+        https://wakatime.com/developers#stats
+
+        Returns:
+            dict: Statistics
+        
+        Raises:
+            KeyError: If session is None
+        """
         return self.session.get("users/current/stats").json()
 
     @staticmethod
     def new_refresh_session(refresh):
+        """Decorator for refresh session
+
+        Args:
+            refresh (func): Function for refresh session
+
+        Returns:
+            func: Wrapper for refresh session
+        """
         def wrapper(self, refresh_token):
             self._new_refresh_session(refresh_token)
             return refresh(self, refresh_token)
@@ -101,22 +199,71 @@ class WakatimeStats(WakatimeAPI):
 
     @new_refresh_session
     async def get_lang_stats(self, refresh_token):
-        """Статистика по языкам"""
+        """Get statistics by languages
+
+        https://wakatime.com/developers#stats
+
+        Args:
+            refresh_token (str): Refresh token
+
+        Returns:
+            dict: Statistics by languages
+        """
 
         return self.get_stats()["data"]["languages"]
 
     @new_refresh_session
     async def get_all_time(self, refresh_token):
+        """Get all time
+
+        https://wakatime.com/developers#stats
+
+        Args:
+            refresh_token (str): Refresh token
+
+        Returns:
+            str: All time
+        """
         return self.get_stats()["data"]["human_readable_total_including_other_language"]
 
     @new_refresh_session
     async def get_os_stats(self, refresh_token):
+        """Get statistics by operating systems
+
+        https://wakatime.com/developers#stats
+
+        Args:
+            refresh_token (str): Refresh token
+
+        Returns:
+            dict: Statistics by operating systems
+        """
         return self.get_stats()["data"]["operating_systems"]
 
     @new_refresh_session
     async def get_editors_stats(self, refresh_token):
+        """Get statistics by editors
+
+        https://wakatime.com/developers#stats
+
+        Args:
+            refresh_token (str): Refresh token
+
+        Returns:
+            dict: Statistics by editors
+        """
         return self.get_stats()["data"]["editors"]
 
     @new_refresh_session
     async def get_categories_stats(self, refresh_token):
+        """Get statistics by categories
+
+        https://wakatime.com/developers#stats
+
+        Args:
+            refresh_token (str): Refresh token
+
+        Returns:
+            dict: Statistics by categories
+        """
         return self.get_stats()["data"]["categories"]
